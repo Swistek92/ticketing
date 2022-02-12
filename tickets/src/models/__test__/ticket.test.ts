6416b595cbb8fef1b8ceaeca962d8bcd63614020
swistek <swistekxd@gmail.com>
2022-02-12 15:05:14 +0100

    test

--- tickets/src/models/__test__/ticket.test.ts
+++ tickets/src/models/__test__/ticket.test.ts
@@ -1,35 +1,5 @@
 import { Ticket } from '../ticket';
 
-it('impelent optimistic cocurrency control', async (done) => {
-  //create an instance of a ticket
-
-  const ticket = Ticket.build({
-    title: 'concert',
-    price: 5,
-    userId: '123',
-  });
-
-  //save the ticket to the database
-  const firstInstance = await Ticket.findById(ticket.id);
-  const secoundInstance = await Ticket.findById(ticket.id);
-
-  //fetch the ticket twice
-  firstInstance!.set({ price: 10 });
-  secoundInstance!.set({ price: 15 });
-  // make two spearate changes to the ticket we fetch
-
-  await firstInstance!.save();
-
-  //save the first fetched ticket
-
-  try {
-    await secoundInstance!.save();
-  } catch (err) {
-    return done();
-  }
-  throw new Error('sould not reach this point');
-});
-
 //  try {
 //     await secondInstance!.save();
 //   } catch (err) {

04173c9d65b72818c1a2090b1f9da2df7b8d2aa2
swistek <swistekxd@gmail.com>
2022-02-19 02:30:45 +0100

    ads

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,16 +4,16 @@ const buildClient = ({ req }) => {
   if (typeof window === 'undefined') {
     //we are on the server..
 
-    // return axios.create({
-    //   baseURL: 'http://www.ticketing-swistek-prod.xyz',
-    //   headers: req.headers,
-    // });
-
     return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://www.ticketing-swistek-prod.xyz',
       headers: req.headers,
     });
+
+    // return axios.create({
+    //   baseURL:
+    //     'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+    //   headers: req.headers,
+    // });
   } else {
     return axios.create({
       baseURL: '/',
